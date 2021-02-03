f5b3e8df3e571720021aa3bcb3d216ff5c9b722b
James Eland <s210209@students.aie.edu.au>
2021-02-03 16:10:13 +1100

    Hello There

---
+++ AIE_01_RaylibPong/AIE_01_RaylibPong/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Raylib_cs;
+
+namespace AIE_01_RaylibPong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 450, "Pong");
+
+            while(Raylib.WindowShouldClose())
+            {
+                //UPDATE Logic
+
+                //DRAWING Logic
+                Raylib.BeginDrawing();
+                Raylib.EndDrawing();
+                
+
+
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
